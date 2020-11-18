f8a6f02bda7ee65bf16bbf5cd513d81631568684
Wellington Guedes <juniozguedes@gmail.com>
2020-11-18 01:27:25 -0300

    Imported routes and bodyParser to server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import routes from './routes'
 
 const app = express()
+app.use(express.json())
+app.use(routes)
 
 app.get('/', (request, response) => {
   return response.json({return: 'browser'})
 })
 
+
 app.listen(3333, ()=>{
   console.log('monkeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
 });
+
